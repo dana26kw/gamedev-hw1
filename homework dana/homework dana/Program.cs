8ba5a81000f2c8e4315f0680087007d2b3f2dd91
dana26kw <dana1a2b3c@gmail.com>
2022-03-11 22:27:02 +0300

    i did my first homework

---
+++ homework dana/homework dana/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace homework_dana
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string charctername = "hero";
+            int playerHealth = 100;
+            int update = 10;
+            int Damage()
+            {
+                
+                int enemyDamage = 20;
+                return playerHealth - enemyDamage;
+            }
+            Console.WriteLine(Damage());
+
+            Console.WriteLine("health is {0}" ,playerHealth+update);
+
+
+               
+           
+
+               
+            
+             int boost()
+            {
+                int boost = 10;
+              return playerHealth + boost;
+            }
+
+            Console.WriteLine(boost());
+
+
+
+
+
+                string[] charcternames = {"80" , "110","120" };
+            Console.WriteLine(charcternames[1]);
+            Console.WriteLine(Damage()>boost());
+            
+
+
+
+
+        }
+
+
+
+        }
+    }
+
